bcc5cc4d583aa711ce261071bd4008e62d62e480
canabhan <canabhan@gmail.com>
2024-04-10 20:06:56 +0530

    revert

--- src/component/ketopro/KetoPro.jsx
+++ src/component/ketopro/KetoPro.jsx
@@ -22,109 +22,79 @@ const KetoPro = () => {
   //   }
   // };
 
-  // const formRef = useRef(null)
-  // const scriptUrl = "https://script.google.com/macros/s/AKfycbxC-p53TGK7Y-rizxOxVUc01W1Sk6leZy7KXfszMiBfe8ckGZmkNLB93x0WUqn5P0srkw/exec"
-  // const [loading, setLoading] = useState(false)
-  // const[loadingData,setLoadingData]=useState("loading")
+  const formRef = useRef(null)
+  const scriptUrl = "https://script.google.com/macros/s/AKfycbxC-p53TGK7Y-rizxOxVUc01W1Sk6leZy7KXfszMiBfe8ckGZmkNLB93x0WUqn5P0srkw/exec"
+  const [loading, setLoading] = useState(false)
+  const[loadingData,setLoadingData]=useState("loading")
 
-  // const handleSubmit = async (e) =>{
-  //     e.preventDefault()
-  //     setLoading(true)
-  //     console.log(formRef.current.Email.value);
-  //     const users = await axios.get( 
-  //       "https://script.google.com/macros/s/AKfycbxC-p53TGK7Y-rizxOxVUc01W1Sk6leZy7KXfszMiBfe8ckGZmkNLB93x0WUqn5P0srkw/exec?action=getData"
-  //      ); 
-  //      console.log(users);
-  //     const formEmail=formRef.current.Email.value;
-  //     const email= users.data.find(user=>user.Email===formEmail)
+  const handleSubmit = async (e) =>{
+      e.preventDefault()
+      setLoading(true)
+      console.log(formRef.current.Email.value);
+      const users = await axios.get( 
+        "https://script.google.com/macros/s/AKfycbxC-p53TGK7Y-rizxOxVUc01W1Sk6leZy7KXfszMiBfe8ckGZmkNLB93x0WUqn5P0srkw/exec?action=getData"
+       ); 
+       console.log(users);
+      const formEmail=formRef.current.Email.value;
+      const email= users.data.find(user=>user.Email===formEmail)
 
-  //     if (!email){
-  //       fetch(scriptUrl, {
-  //         method: 'POST', 
-  //         body: new FormData(formRef.current),
+      if (!email){
+        fetch(scriptUrl, {
+          method: 'POST', 
+          body: new FormData(formRef.current),
     
-  //     }).then(res => {
-  //             console.log("SUCCESSFULLY SUBMITTED", res)
-  //             setLoadingData('success')
-  //         })
-  //         .catch(err => console.log(err))
-  //     }else{
-  //       setLoadingData('suc')
-  //     }
+      }).then(res => {
+              console.log("SUCCESSFULLY SUBMITTED", res)
+              setLoadingData('success')
+          })
+          .catch(err => console.log(err))
+      }else{
+        setLoadingData('suc')
+      }
     
-  // }
+  }
 
   return (
     <div className='keto'>
-      {/* { loading&& 
+      { loading&& 
       <Loader data={loadingData} setLoading={setLoading}/>
-    } */}
+    }
       <div className="container">
             <div className="ketopro">
-             <h2>Best Articles for You </h2>
+             <h2>Get Keto Products</h2>
               <table>
               <tr>
-                  <td>WHAT MEN SECRETLY WANT </td>
-                  <td className='btn'> <a target='_blank' href="https://tinyurl.com/bestarticlesforyou">click here</a></td>
+                  <td>Custom Keto Diet</td>
+                  <td className='btn'> <a target='_blank' href="https://www.digistore24.com/redir/434557/nabhanca/">click here</a></td>
                 </tr>
                 <tr>
-                  <td>A PRESENTATION OF FREE ATTRACTION TIPS</td>
-                  <td className='btn'> <a target='_blank' href="https://tinyurl.com/bestarticlesforyou">click here</a></td>
+                  <td>The Ultimate Keto Meal Plan</td>
+                  <td className='btn'> <a target='_blank' href="https://www.claudiacaldwell.com/oto-uf61a?el=splittest-1214-bradflow-control#aff=nabhanca">click here</a></td>
                 </tr>
                 <tr>
-                  <td>PRIZING IMPERFECTIONS </td>
-                  <td className='btn'> <a target='_blank' href="https://tinyurl.com/bestarticlesforyou">click here</a></td>
-                </tr>
-                <tr>
-                  <td>BEAUTIFUL PEOPLE VS BEAUTIFUL RELATIONSHIP </td>
-                  <td className='btn'> <a target='_blank' href="https://tinyurl.com/bestarticlesforyou">click here</a></td>
-                </tr>
-                <tr>
-                  <td>STOP GETTING HURT BY UNTRUSTWORTHY MEN </td>
-                  <td className='btn'> <a target='_blank' href="https://tinyurl.com/bestarticlesforyou">click here</a></td>
-                </tr>
-              <tr>
-                  <td> HE DOESN'T REALLY WANT THE PERFECT WOMAN</td>
-                  <td className='btn'> <a target='_blank' href="https://tinyurl.com/bestarticle1">click here</a></td>
-                </tr>
-                <tr>
-                  <td>THE STRANGEST THING MEN DESIRE</td>
-                  <td className='btn'> <a target='_blank' href="https://tinyurl.com/bestarticles2">click here</a></td>
-                </tr>
-                <tr>
-                  <td> THE 1 EASY IDEA YOU CAN USE TODAY</td>
-                  <td className='btn'> <a target='_blank' href="https://tinyurl.com/bestarticles3">click here</a></td>
+                  <td> The Keto Snacks Cookbook</td>
+                  <td className='btn'> <a target='_blank' href="https://www.digistore24.com/redir/400534/nabhanca/">click here</a></td>
                 </tr>               
                 <tr>
-                  <td> WHY MEN PULL AWAY AND HOW YOU CAN TAP INTO HIS DEEP DESIRE</td>
-                  <td className='btn'> <a target='_blank' href="https://tinyurl.com/bestarticles5">click here</a></td>
+                  <td>The Essential Keto Cookbook</td>
+                  <td className='btn'> <a target='_blank' href="https://www.digistore24.com/redir/269221/nabhanca/">click here</a></td>
                 </tr>
                 <tr>
-                  <td>MAKE HIM WANT YOU
-</td>
-                  <td className='btn'> <a target='_blank' href="https://tinyurl.com/bestarticles6">click here</a></td>
+                  <td>The Keto Air Fryer Cookbook</td>
+                  <td className='btn'> <a target='_blank' href="https://www.digistore24.com/redir/411008/nabhanca/">click here</a></td>
                 </tr>
                 <tr>
-                  <td>THE ATTRACTION TRIP-WIRE THAT AWAKENS A MAN'S DEEPEST DESIRE</td>
-                  <td className='btn'> <a target='_blank' href="https://tinyurl.com/bestarticles4">click here</a></td>
+                  <td>Keto Desserts</td>
+                  <td className='btn'> <a target='_blank' href="https://www.digistore24.com/redir/292043/nabhanca/">click here</a></td>
                 </tr>
                 <tr>
-                  <td>WHY MEN PULL AWAY AND HOW YOU CAN TAP INTO HIS DEEP DESIRE</td>
-                  <td className='btn'> <a target='_blank' href="https://tinyurl.com/bestarticles5">click here</a></td>
-                </tr>
-  
-                <tr>
-                  <td>HOW TO GET YOUR EX BACK</td>
-                  <td className='btn'> <a target='_blank' href="https://tinyurl.com/bestarticles7">click here</a></td>
-                </tr>
-                <tr>
-                  <td>HOW TO GET OVER A BREAKUP</td>
-                  <td className='btn'> <a target='_blank' href="https://tinyurl.com/bestarticles8">click here</a></td>
+                  <td>BACK TO BASIC</td>
+                  <td className='btn'> <a target='_blank' href="https://www.digistore24.com/redir/405503/nabhanca/">click here</a></td>
                 </tr>
               </table>
-             {/* <h2>Get Free Ebook</h2> */}
+             <h2>Get Free Ebook</h2>
 
-              {/* <div className="box">
+              <div className="box">
                 <form className='form' ref={formRef} onSubmit={handleSubmit} name="google-sheet">
                 <label for="fname">First Name</label>
                 <input type="text" id='name' name="Name" required placeholder="Your name.."/>
@@ -134,7 +104,7 @@ const KetoPro = () => {
                 <input type="submit" name='submit' value="Get Free Ebook"/>
 
                 </form>
-              </div> */}
+              </div>
             </div>
         </div>
     </div>
