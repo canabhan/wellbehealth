e73b6414c183320c70282f708fdae0249a54a70a
canabhan <canabhan@gmail.com>
2024-04-21 20:21:21 +0530

    v

--- src/component/heroSection/Hero.jsx
+++ src/component/heroSection/Hero.jsx
@@ -9,6 +9,7 @@ const Hero = () => {
 
         <div className='info'>
             <span className='span'>Get Your Ebook</span>
+            
             <h1>The Keto Blueprint: <br /><span> Your Roadmap to Success on the Ketogenic Diet</span> </h1>
         </div>
         </div>
